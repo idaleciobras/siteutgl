c55534260ed6de4380c77d88b6f4f21fc1904d97
idaleciobras <105823356+idaleciobras@users.noreply.github.com>
2024-08-05 18:07:00 +0100

    Create mail.php

---
+++ mail.php
@@ -0,0 +1,20 @@
+<?php
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$telefone = $_POST['telefone'];
+$mensagem = $_POST['mensagem'];
+
+$destinatario = "idaleciobraz399@gmail.com";
+$assunto = "Clientes UTGL";
+
+$corpo = "Nome: ".$nome."\n".Email: ".$email."\n"."Telefone: ".$telefone."\n"."mensagem: ",$mensagem;
+
+$cabeca = "from: idaleciobraz399@gmail.com" ."\n"."Reply-to: ".$email."\n"."X-Mailer:PHP/" .phpversion();
+
+if(mail($destino,$assunto,$corpo,$cabeca)){
+  echo("Email enviado com sucesso");
+}else{
+  echo("Houve um erro a enviar o email");
+
+
+?>
